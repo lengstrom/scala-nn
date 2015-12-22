f082cd40203b550f84dd7c8e9e531d6641b3e17b
lengstrom <lengstrom@users.noreply.github.com>
2015-12-22 14:11:36 -0500

    made constructor

--- src/main/scala/NeuralNetwork.scala
+++ src/main/scala/NeuralNetwork.scala
@@ -2,12 +2,14 @@ import breeze.stats.distributions._
 import breeze.linalg._
 import scala.math._
 
-class NeuralNetwork {
-
+class NeuralNetwork (layers: List[Int]) {
+  val biases : Vector[DenseVector[Double]] = for ( n <- layers) yield
+    new DenseVector[Double](data: Gaussian(mu: 0, sigma: 0).sample(n))
+  println(biases)
 }
 
 object main {
   def main(args: Array[String]) : Unit = {
-    println("Hello world!" + args.foldRight("")(_ + " " + _))
+    println("Runs!" + args.foldRight("")(_ + " " + _))
   }
 }
